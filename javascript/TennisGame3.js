b6e6025890768fcf26d8530917ac96b708994195
michael.passler <michael.passler@andrena.de>
2023-07-26 18:42:20 +0200

    rename p1 to score1 etc

--- javascript/TennisGame3.js
+++ javascript/TennisGame3.js
@@ -1,6 +1,6 @@
 var TennisGame3 = function(p1N, p2N) {
-    this.p2 = 0;
-    this.p1 = 0;
+    this.score2 = 0;
+    this.score1 = 0;
 
     this.p1N = p1N;
     this.p2N = p2N;
@@ -8,23 +8,23 @@ var TennisGame3 = function(p1N, p2N) {
 
 TennisGame3.prototype.getScore = function() {
     var s;
-    if ((this.p1 < 4 && this.p2 < 4) && (this.p1 + this.p2 < 6)) {
+    if ((this.score1 < 4 && this.score2 < 4) && (this.score1 + this.score2 < 6)) {
         var p = ["Love", "Fifteen", "Thirty", "Forty"];
-        s = p[this.p1];
-        return (this.p1 === this.p2) ? s + "-All" : s + "-" + p[this.p2];
+        s = p[this.score1];
+        return (this.score1 === this.score2) ? s + "-All" : s + "-" + p[this.score2];
     } else {
-        if (this.p1 === this.p2)
+        if (this.score1 === this.score2)
             return "Deuce";
-        s = this.p1 > this.p2 ? this.p1N : this.p2N;
-        return ((this.p1 - this.p2) * (this.p1 - this.p2) === 1) ? "Advantage " + s : "Win for " + s;
+        s = this.score1 > this.score2 ? this.p1N : this.p2N;
+        return ((this.score1 - this.score2) * (this.score1 - this.score2) === 1) ? "Advantage " + s : "Win for " + s;
     }
 };
 
 TennisGame3.prototype.wonPoint = function(playerName) {
     if (playerName === "player1")
-        this.p1 += 1;
+        this.score1 += 1;
     else
-        this.p2 += 1;
+        this.score2 += 1;
 
 };
 
