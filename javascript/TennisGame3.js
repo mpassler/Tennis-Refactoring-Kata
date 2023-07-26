ddee443dba7da9a8a2e23c9def0075f5cb953766
michael.passler <michael.passler@andrena.de>
2023-07-26 18:40:27 +0200

    replace == with ===

--- javascript/TennisGame3.js
+++ javascript/TennisGame3.js
@@ -11,17 +11,17 @@ TennisGame3.prototype.getScore = function() {
     if ((this.p1 < 4 && this.p2 < 4) && (this.p1 + this.p2 < 6)) {
         var p = ["Love", "Fifteen", "Thirty", "Forty"];
         s = p[this.p1];
-        return (this.p1 == this.p2) ? s + "-All" : s + "-" + p[this.p2];
+        return (this.p1 === this.p2) ? s + "-All" : s + "-" + p[this.p2];
     } else {
-        if (this.p1 == this.p2)
+        if (this.p1 === this.p2)
             return "Deuce";
         s = this.p1 > this.p2 ? this.p1N : this.p2N;
-        return ((this.p1 - this.p2) * (this.p1 - this.p2) == 1) ? "Advantage " + s : "Win for " + s;
+        return ((this.p1 - this.p2) * (this.p1 - this.p2) === 1) ? "Advantage " + s : "Win for " + s;
     }
 };
 
 TennisGame3.prototype.wonPoint = function(playerName) {
-    if (playerName == "player1")
+    if (playerName === "player1")
         this.p1 += 1;
     else
         this.p2 += 1;
