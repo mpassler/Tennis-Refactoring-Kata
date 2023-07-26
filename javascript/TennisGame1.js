e0261e0bd8de48d163a1318b3a9ed17d5ceb0c27
michael.passler <michael.passler@andrena.de>
2023-07-26 17:50:54 +0200

    move getscore method in class

--- javascript/TennisGame1.js
+++ javascript/TennisGame1.js
@@ -12,57 +12,59 @@ class TennisGame1 {
         else
             this.m_score2 += 1;
     };
-}
 
-TennisGame1.prototype.getScore = function() {
-    var score = "";
-    var tempScore = 0;
-    if (this.m_score1 === this.m_score2) {
-        switch (this.m_score1) {
-            case 0:
-                score = "Love-All";
-                break;
-            case 1:
-                score = "Fifteen-All";
-                break;
-            case 2:
-                score = "Thirty-All";
-                break;
-            default:
-                score = "Deuce";
-                break;
-        }
-    } else if (this.m_score1 >= 4 || this.m_score2 >= 4) {
-        var minusResult = this.m_score1 - this.m_score2;
-        if (minusResult === 1) score = "Advantage player1";
-        else if (minusResult === -1) score = "Advantage player2";
-        else if (minusResult >= 2) score = "Win for player1";
-        else score = "Win for player2";
-    } else {
-        for (var i = 1; i < 3; i++) {
-            if (i === 1) tempScore = this.m_score1;
-            else {
-                score += "-";
-                tempScore = this.m_score2;
-            }
-            switch (tempScore) {
+    getScore = function() {
+        var score = "";
+        var tempScore = 0;
+        if (this.m_score1 === this.m_score2) {
+            switch (this.m_score1) {
                 case 0:
-                    score += "Love";
+                    score = "Love-All";
                     break;
                 case 1:
-                    score += "Fifteen";
+                    score = "Fifteen-All";
                     break;
                 case 2:
-                    score += "Thirty";
+                    score = "Thirty-All";
                     break;
-                case 3:
-                    score += "Forty";
+                default:
+                    score = "Deuce";
                     break;
             }
+        } else if (this.m_score1 >= 4 || this.m_score2 >= 4) {
+            var minusResult = this.m_score1 - this.m_score2;
+            if (minusResult === 1) score = "Advantage player1";
+            else if (minusResult === -1) score = "Advantage player2";
+            else if (minusResult >= 2) score = "Win for player1";
+            else score = "Win for player2";
+        } else {
+            for (var i = 1; i < 3; i++) {
+                if (i === 1) tempScore = this.m_score1;
+                else {
+                    score += "-";
+                    tempScore = this.m_score2;
+                }
+                switch (tempScore) {
+                    case 0:
+                        score += "Love";
+                        break;
+                    case 1:
+                        score += "Fifteen";
+                        break;
+                    case 2:
+                        score += "Thirty";
+                        break;
+                    case 3:
+                        score += "Forty";
+                        break;
+                }
+            }
         }
-    }
-    return score;
-};
+        return score;
+    };
+
+}
+
 
 if (typeof window === "undefined") {
     module.exports = TennisGame1;
